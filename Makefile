9e812a3df586caf2707c4476a5918424b80195b0
Rbate <98659534+NeilRbate@users.noreply.github.com>
2023-06-14 21:26:22 +0200

    Add starting main.cpp + arg parsing

---
+++ Makefile
@@ -0,0 +1,22 @@
+CXX = c++
+SRCS = main.cpp
+OBJS = ${SRCS:.cpp=.o}
+NAME = ircserv
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98
+
+.c.o:
+	${CXX} ${CFLAGS} -c $< -o ${<:.c=.o}
+
+${NAME}: ${OBJS}
+	${CXX} ${CXXFLAGS} -o ${NAME} ${OBJS}
+
+all: ${OBJS} ${NAME}
+
+clean:
+	@rm -f ${OBJS}
+	@echo Everything is clean
+
+fclean: clean
+	@rm -f ${NAME}
+
+re: fclean all
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+int	main(int argc, char **argv) {
+
+	if (argc != 3) {
+		std::cerr << "Error: invalid number of arguments !" << std::endl;
+		return (1);
+	}
+	std::string	port(argv[1]), password(argv[2]);
+	if (port.empty() || password.empty() || \
+		port.find_first_not_of("0123456789") != std::string::npos) {
+		std::cerr << "Error: invalid arguments !" << std::endl;
+	}
+	return (0);
+}
